ea038f356a4a74be227d479eb0ee47498e8bdfba
Dmitry <dima.shevchenko10@gmail.com>
2024-06-09 01:23:49 +0300

    request status was moved to approvalRequest entity

---
+++ OutOfOffice.DAL/Migrations/20240608222203_Move_RequestStatus_Migration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutOfOffice.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Move_RequestStatus_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "LeaveRequests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ApprovalRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ApprovalRequests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "LeaveRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
